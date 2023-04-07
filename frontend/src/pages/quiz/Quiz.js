977a62e7ace05e5cb7749e0503979737918f8b1f
Eitvydas Jankauskas <70910743+eitjank@users.noreply.github.com>
2023-04-07 21:29:05 +0300

    Get quiz questions from api, show questions and show score after answering

--- frontend/src/pages/quiz/Quiz.js
+++ frontend/src/pages/quiz/Quiz.js
@@ -1,45 +1,33 @@
 import React, {useEffect, useState} from "react";
 import "./Quiz.css";
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import axios from "axios";
 
 function Quiz() {
     const [quiz, setQuiz] = useState(null);
     const {id} = useParams();
-    const [questions, setQuestions] = useState([
-        {
-            question: "What is the capital of France?",
-            options: ["Paris", "Madrid", "London", "Berlin"],
-            answer: "Paris"
-        },
-        {
-            question: "What is the largest country in the world?",
-            options: ["USA", "China", "Russia", "India"],
-            answer: "Russia"
-        },
-        {
-            question: "What is the currency of Japan?",
-            options: ["Yen", "Dollar", "Euro", "Pound"],
-            answer: "Yen"
-        }
-    ]);
-
+    const [questions, setQuestions] = useState([]);
     const [currentQuestion, setCurrentQuestion] = useState(0);
     const [score, setScore] = useState(0);
     const [showScore, setShowScore] = useState(false);
+    const navigate = useNavigate();
 
     useEffect(() => {
-        axios.get(`http://localhost:8080/api/quizzes/${id}`)
-            .then(response => setQuiz(response.data))
+        axios.get(`http://localhost:8080/api/questions/quiz/${id}`)
+            .then(response => {
+                setQuiz(response.data[0].quiz);
+                setQuestions(response.data);
+            })
             .catch((error) => console.log(error));
     }, [id]);
 
-    if (!quiz) {
+
+    if (!questions || questions.length === 0) { // Check if questions are loaded
         return <div>Loading quiz...</div>;
     }
 
     const handleAnswerOptionClick = (answer) => {
-        if (answer === questions[currentQuestion].answer) {
+        if (answer === questions[currentQuestion].answerIndex) {
             setScore(score + 1);
         }
 
@@ -51,40 +39,40 @@ function Quiz() {
         }
     };
 
+    function handleBackButtonClick() {
+        navigate("/");
+    }
+
     return (
         <div>
-            <h2>{quiz.name}</h2>
-            {/* Add quiz questions and answer options here */}
+            <h2>{quiz && quiz.name}</h2>
+            <button className="back-button" onClick={handleBackButtonClick}>Back To Main Page</button>
+            {showScore ? (
+                <div className="score-section">
+                    <h2>You scored {score} out of {questions.length}</h2>
+                </div>
+            ) : (
+                <>
+                    <div className="question-section">
+                        <h2 className="question-count">
+                            Question {currentQuestion + 1} of {questions.length}
+                        </h2>
+                        <div className="question-text">
+                            {questions[currentQuestion].text}
+                        </div>
+                    </div>
+                    <div className="answer-section">
+                        {questions[currentQuestion].options.map((option, index) => (
+                            <button key={index} className="answer-button"
+                                    onClick={() => handleAnswerOptionClick(index)}>
+                                {option.value}
+                            </button>
+                        ))}
+                    </div>
+                </>
+            )}
         </div>
     );
-
-    // return (
-    //     <div className="quiz">
-    //         {showScore ? (
-    //             <div className="score-section">
-    //                 <h2>You scored {score} out of {questions.length}</h2>
-    //             </div>
-    //         ) : (
-    //             <>
-    //                 <div className="question-section">
-    //                     <h2 className="question-count">
-    //                         Question {currentQuestion + 1} of {questions.length}
-    //                     </h2>
-    //                     <div className="question-text">
-    //                         {questions[currentQuestion].question}
-    //                     </div>
-    //                 </div>
-    //                 <div className="answer-section">
-    //                     {questions[currentQuestion].options.map((option) => (
-    //                         <button className="answer-button" onClick={() => handleAnswerOptionClick(option)}>
-    //                             {option}
-    //                         </button>
-    //                     ))}
-    //                 </div>
-    //             </>
-    //         )}
-    //     </div>
-    // );
 }
 
 export default Quiz;
