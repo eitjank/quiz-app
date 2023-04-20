037e21323e19efa98fed27cc1173cee893bd276c
Eitvydas Jankauskas <70910743+eitjank@users.noreply.github.com>
2023-04-20 19:39:20 +0300

    Refactor: Delete unnecessary code

--- backend/src/main/java/lt/quiz/backend/quiz/question/option/OptionRepository.java
+++
@@ -1,9 +0,0 @@
-package lt.quiz.backend.quiz.question.option;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface OptionRepository extends JpaRepository<Option, Long> {
-    List<Option> findByQuestionId(Long questionId);
-}
